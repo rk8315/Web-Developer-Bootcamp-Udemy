f2ae7740a9500c4caaccf2806ead8d707a6eff8f
Bob K <kearnsr135@gmail.com>
2019-01-03 12:37:30 -0500

    complete JS stalker exercise

---
+++ Section10-Intro to JavaScript/JS-Stalker-Exercise/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+console.log("Nice to meet you, " + firstName + " " + lastName + "! It must be great to be " + userAge + " years old!")
---
+++ Section10-Intro to JavaScript/scriptDemo/script.js
@@ -0,0 +1,5 @@
+alert("hello from js file");
+
+var userName = prompt("What is your name?");
+
+alert("Nice to meet you, " + userName);
