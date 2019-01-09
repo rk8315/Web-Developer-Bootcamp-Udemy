11791d118c92e73d07717703714837d9191a6b6a
Bob K <kearnsr135@gmail.com>
2019-01-09 17:17:18 -0500

    complete todo list code along part2

--- Section13-JS Arrays/ToDo-List-App/list.js
+++ Section13-JS Arrays/ToDo-List-App/list.js
@@ -6,16 +6,41 @@ window.setTimeout(function() {
   while(input !== "quit"){
     //handle input
     if(input === "list"){
-      console.log(todos);
+      listTodos();
     } else if (input === "new"){
-      // ask for new todo
-      var newTodo = prompt("Enter new todo");
-      // add to todos array
-      todos.push(newTodo);
+      addTodo();
+    } else if(input === "delete"){
+      deleteTodo();
     }
     //ask again for new input
     input = prompt("What would you like to do?");
 
   }
-  console.log("OK YOU QUIT THE APP");
+  console.log("OK, YOU QUIT THE APP");
+
+
+  function addTodo(){
+    // ask for new todo
+    var newTodo = prompt("Enter new todo");
+    // add to todos array
+    todos.push(newTodo);
+    console.log("Added todo");
+  }
+
+  function deleteTodo(){
+    // ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    // delete the todo
+    todos.splice(index, 1);
+    console.log("Deleted todo");
+  }
+
+  function listTodos(){
+    console.log("**********")
+    todos.forEach(function(todo, index){
+      console.log(index + ": " + todo);
+    });
+    console.log("**********")
+  }
+
 }, 500);
