8e32e36d6bed3f0dbdb77fa55456bcf9893cb27c
Bob K <kearnsr135@gmail.com>
2019-01-13 13:26:40 -0500

    completed score keeper

--- Section16-Adv DOM Manipulation/Score-Keeper/script.js
+++ Section16-Adv DOM Manipulation/Score-Keeper/script.js
@@ -1,7 +1,10 @@
 var p1Button = document.querySelector('#p1');
 var p2Button = document.querySelector('#p2');
+var resetButton = document.querySelector('#reset')
 var p1Display = document.querySelector('#p1Display');
 var p2Display = document.querySelector('#p2Display');
+var numInput = document.querySelector('input');
+var winningScoreDisplay = document.querySelector('p span');
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -12,6 +15,7 @@ p1Button.addEventListener("click", function(){
     p1Score++;
     if(p1Score === winningScore){
       gameOver = true;
+      p1Display.classList.add("winner")
     }
     p1Display.textContent = p1Score;
   }
@@ -22,7 +26,26 @@ p2Button.addEventListener("click", function(){
     p2Score++;
     if(p2Score === winningScore){
       gameOver = true;
+      p2Display.classList.add("winner")
     }
     p2Display.textContent = p2Score;
   }
+})
+
+resetButton.addEventListener("click", reset)
+
+function reset(){
+  p1Score = 0;
+  p1Display.textContent = 0;
+  p2Score = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+  winningScoreDisplay.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
 })
